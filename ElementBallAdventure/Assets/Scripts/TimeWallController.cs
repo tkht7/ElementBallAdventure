f5fb65759a3fc9c4d100e2ffab8fb3d8b5948c1b
tkht <takahito711@gmail.com>
2020-02-22 19:22:43 +0900

    ステージ2作成途中

--- ElementBallAdventure/Assets/Scripts/TimeWallController.cs
+++ ElementBallAdventure/Assets/Scripts/TimeWallController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TimeWallController : MonoBehaviour
 {
-    private GameObject pivot;
+    private GameObject pivot; // 壁の基準位置
     private GameObject button;
     private bool pushFlag;
     Vector3 startPivotPos;
@@ -23,14 +23,14 @@ public class TimeWallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pushFlag)
+        if (pushFlag) // ボタンが押されてるときは壁を上げて，ボタンを下げる(押されてる感じにする)
         {
-            if(startButtonPos.y - button.transform.position.y < 0.2f)
+            if(startButtonPos.y - button.transform.position.y < 0.2f) // 押し込みの制限
             {
                 var tempPos = button.transform.position;
                 button.transform.position = new Vector3(tempPos.x, tempPos.y - 0.01f, tempPos.z);
             }
-            if(pivot.transform.position.y - startPivotPos.y < 8.0f)
+            if(pivot.transform.position.y - startPivotPos.y < 8.0f) // 壁を上げる位置の制限
             {
                 var tempPos = pivot.transform.position;
                 pivot.transform.position = new Vector3(tempPos.x, tempPos.y + 0.1f, tempPos.z);
@@ -38,15 +38,36 @@ public class TimeWallController : MonoBehaviour
         }
         else
         {
-            if (startButtonPos.y - button.transform.position.y > 0.0f)
+            if (startButtonPos.y - button.transform.position.y > 0.0f) // 元の位置まで戻るようにする
             {
                 var tempPos = button.transform.position;
                 button.transform.position = new Vector3(tempPos.x, tempPos.y + 0.01f, tempPos.z);
             }
-            if (pivot.transform.position.y - startPivotPos.y > 0.0f)
+            if (pivot.transform.position.y - startPivotPos.y > 0.0f) // 元の位置以上に下がらないようにする
             {
                 var tempPos = pivot.transform.position;
                 pivot.transform.position = new Vector3(tempPos.x, tempPos.y - 0.02f, tempPos.z);
+            }
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            // 壁につぶされて地面をすり抜けるのを防ぐ
+            if(Mathf.Abs(collision.gameObject.transform.position.x - transform.position.x) < 0.6f && // 壁の真下付近にプレイヤーがいたとき
+               pivot.transform.position.y - startPivotPos.y < 1.0f) // 壁が閉まりかけの時
+            {
+                var dir = 0.0f;
+                if (collision.gameObject.transform.position.x - transform.position.x < 0)
+                    dir = -1.0f;
+                else
+                    dir = 1.0f;
+                var tempPos = collision.gameObject.transform.position;
+                collision.gameObject.transform.position = new Vector3(pivot.transform.position.x+dir, tempPos.y, tempPos.z); // 壁のない位置にプレイヤーを移動
+
+                // 違う向きで配置するときはyについて同じことをする
             }
         }
     }
