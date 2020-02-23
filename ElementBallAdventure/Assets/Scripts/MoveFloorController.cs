54225ba38c6f52bbfc4e4da46258304cbcf40ba1
tkht <takahito711@gmail.com>
2020-02-24 03:40:25 +0900

    ステージ3作成中

--- ElementBallAdventure/Assets/Scripts/MoveFloorController.cs
+++ ElementBallAdventure/Assets/Scripts/MoveFloorController.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 
 public class MoveFloorController : MonoBehaviour
 {
-    public int moveDirect; // 0: X方向, 1: Y方向, 2: Z方向
+    public bool[] moveDirect = new bool[3];
+    public bool[] reverse = new bool[3];
     public float moveDistance;
     public float moveSpeed;
-    public bool reverse;
 
     private Rigidbody rb;
-    private float rev;
-    private Vector3 startPos;
+    private float[] rev = { 1.0f, 1.0f, 1.0f };
+    private float[] startPos = new float[3];
+    private float[] updatePos = new float[3];
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        startPos = transform.position;
-        rev = 1.0f;
-        if (reverse)
-            rev = -1.0f;
-
-    }
+        startPos[0] = transform.position.x;
+        startPos[1] = transform.position.y;
+        startPos[2] = transform.position.z;
+        for (int i = 0; i < reverse.Length; i++)
+        {
+            if (reverse[i])
+                rev[i] = -1.0f;
+            updatePos[i] = startPos[i];
+        }
+}
 
     // Update is called once per frame
     void Update()
     {
-        if (moveDirect == 0)
-            rb.MovePosition(new Vector3(startPos.x + rev * Mathf.PingPong(Time.time * moveSpeed, moveDistance), startPos.y, startPos.z));
-        else if (moveDirect == 1)
-            rb.MovePosition(new Vector3(startPos.x, startPos.y + rev * Mathf.PingPong(Time.time * moveSpeed, moveDistance), startPos.z));
-        else if (moveDirect == 2)
-            rb.MovePosition(new Vector3(startPos.x, startPos.y, startPos.z + rev * Mathf.PingPong(Time.time * moveSpeed, moveDistance)));
-
+        for (int i = 0; i < moveDirect.Length; i++)
+        {
+            if (moveDirect[i])
+                updatePos[i] = startPos[i] + rev[i] * Mathf.PingPong(Time.time * moveSpeed, moveDistance);
+        }
+        rb.MovePosition(new Vector3(updatePos[0], updatePos[1], updatePos[2]));
     }
 }
